7d3b5fa746b8203998240d53e33e0cb094390358
Okamium <lucabtei@outlook.com>
2024-10-19 20:51:39 -0300

    Lógica do ranking e imagens

--- ranking.php
+++ ranking.php
@@ -33,7 +33,79 @@
     </nav>
 
     <?php
-        echo $_SESSION['genero'];
+        echo "<br>";
+
+        $seriesTerror = ["The Haunting of Hill House", "American Horror Story", "The Walking Dead", "Stranger Things", "Penny Dreadful", "The Exorcist"];
+        $imagensTerror = ["haunting-of-hill-house.jpg", "american-horror-story.webp", "twd.webp",  "stranger-things.jpg",  "penny-dreadful.jpg",  "exorcist.jpg"];
+
+        $seriesAcao = ["24 Horas", "Jack Ryan", "Arrow", "The Mandalorian", "Vikings", "The Witcher"];
+        $imagensAcao = [""];
+
+        $seriesComedia = ["Friends", "The Office", "Brooklyn Nine-Nine", "Parks and Recreation", "How I Met Your Mother", "The Big Bang Theory"];
+        $imagensComedia = [""];
+
+        $seriesDrama = ["Breaking Bad", "The Crown", "The Handmaid's Tale", "This Is Us", "Succession", "Mad Men"];
+        $imagensComedia = [""];
+        
+        $menuItems = 6;
+        
+        switch($_SESSION['genero']) {
+            case 'Terror':
+                for ($i = 0; $i < $menuItems; $i++) {
+                    echo "<div class='card'>
+                            <img src='assets/terror/" . $imagensTerror[$i] . "' alt='Terror' class='card-image'>
+                            <div class='card-content'>
+                                <h2>" . $seriesTerror[$i] . "</h2>
+                            </div>
+                        </div>
+                        <br>
+                        ";
+                }
+                break;
+
+            case 'Ação':
+                for ($i = 0; $i < $menuItems; $i++) {
+                echo "<div class='card'>
+                        <img src='assets/terror.webp' alt='Terror' class='card-image'>
+                        <div class='card-content'>
+                            <h2>" . $seriesAcao[$i] . "</h2>
+                        </div>
+                    </div>
+                    <br>
+                    ";
+                }
+                break;
+
+            case 'Comédia':
+                for ($i = 0; $i < $menuItems; $i++) {
+                echo "<div class='card'>
+                        <img src='assets/terror.webp' alt='Terror' class='card-image'>
+                        <div class='card-content'>
+                            <h2>" . $seriesComedia[$i] . "</h2>
+                        </div>
+                    </div>
+                    <br>
+                    ";
+                }
+                break;
+
+            case 'Drama':
+                for ($i = 0; $i < $menuItems; $i++) {
+                echo "<div class='card'>
+                        <img src='assets/terror.webp' alt='Terror' class='card-image'>
+                        <div class='card-content'>
+                            <h2>" . $seriesDrama[$i] . "</h2>
+                        </div>
+                    </div>
+                    <br>
+                    ";
+                }
+                break;
+
+            default:
+                echo "Opção inválida";
+                break;
+        }
     ?>
 </body>
 </html>
